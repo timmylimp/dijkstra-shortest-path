f4c4aed92af55b99371dc32694ccb361a34e9e68
timmylimp <timmy.limp@gmail.com>
2018-07-24 14:58:44 +0700

    Add node and edge validation unit test.

--- Dijkstra/Dijkstra.Graph.UnitTest/EdgeUnitTest.cs
+++ Dijkstra/Dijkstra.Graph.UnitTest/EdgeUnitTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Dijkstra.Graph.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dijkstra.Graph.UnitTest
 {
@@ -6,8 +9,63 @@ namespace Dijkstra.Graph.UnitTest
     public class EdgeUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Node_TryValidate_NoSource_Invalid()
         {
+            var edge = new Edge { Destination = new Node(), Weight = 1 };
+            List<ValidationResult> validationResults;
+
+            Assert.IsFalse(edge.TryValidate(out validationResults));
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The Source field is required.", validationResults[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_NoDestination_Invalid()
+        {
+            var edge = new Edge { Source = new Node(), Weight = 1 };
+            List<ValidationResult> validationResults;
+
+            Assert.IsFalse(edge.TryValidate(out validationResults));
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The Destination field is required.", validationResults[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_WeightLessThan0_Invalid()
+        {
+            var edge = new Edge { Source = new Node(), Destination = new Node(), Weight = -1 };
+            List<ValidationResult> validationResults;
+
+            Assert.IsFalse(edge.TryValidate(out validationResults));
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The field Weight must be greater or equal 0.", validationResults[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_FullDataWeightIs0_Valid()
+        {
+            var edge = new Edge { Source = new Node(), Destination = new Node(), Weight = 0 };
+            List<ValidationResult> validationResults;
+
+            Assert.IsTrue(edge.TryValidate(out validationResults));
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_FullDataWeightIs10_Valid()
+        {
+            var edge = new Edge { Source = new Node(), Destination = new Node(), Weight = 10 };
+            List<ValidationResult> validationResults;
+
+            Assert.IsTrue(edge.TryValidate(out validationResults));
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_FullDataWeightIsMaxInt_Valid()
+        {
+            var edge = new Edge { Source = new Node(), Destination = new Node(), Weight = int.MaxValue };
+            List<ValidationResult> validationResults;
+
+            Assert.IsTrue(edge.TryValidate(out validationResults));
         }
     }
 }
--- Dijkstra/Dijkstra.Graph.UnitTest/NodeUnitTest.cs
+++ Dijkstra/Dijkstra.Graph.UnitTest/NodeUnitTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Dijkstra.Graph.Utils;
 
 namespace Dijkstra.Graph.UnitTest
 {
@@ -6,8 +9,23 @@ namespace Dijkstra.Graph.UnitTest
     public class NodeUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Node_TryValidate_NoName_Invalid()
         {
+            var node = new Node { Name = "" };
+            List<ValidationResult> validationResults;
+
+            Assert.IsFalse(node.TryValidate(out validationResults));
+            Assert.AreEqual(1, validationResults.Count);
+            Assert.AreEqual("The Name field is required.", validationResults[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Node_TryValidate_WithName_Invalid()
+        {
+            var node = new Node { Name = "Timmy" };
+            List<ValidationResult> validationResults;
+
+            Assert.IsTrue(node.TryValidate(out validationResults));
         }
     }
 }
